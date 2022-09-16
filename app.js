1ea6704666f5772e5ce5abac04589e02b25d7be5
Praneetha Charugundla <88439139+PraneethaCharugundla@users.noreply.github.com>
2022-09-16 17:47:30 +0530

    update: styles

---
+++ app.js
@@ -0,0 +1,40 @@
+var btntranslate = document.querySelector("#btn-translate")
+
+var txtInput =document.querySelector("#txt-input");
+var outputDiv= document.querySelector("#output");
+var profilePic = document.querySelector("#profile-pic");
+
+
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "	https://api.funtranslations.com/translate/groot.json"
+
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+    console.log("error occured", error)
+    alert("something wrong with server ! try after sometime")
+}
+
+   
+function clickHandler() {
+ var inputText = txtInput.value; //taking input
+
+ fetch(getTranslationURL(inputText))
+   .then(reponse => reponse.json())
+   .then(json => {
+var tarnslatedText = json.contents.translated;
+    outputDiv.innerText = tarnslatedText; //output
+   })
+    .catch(errorHandler)
+
+};
+
+btntranslate.addEventListener("click",clickHandler)
+
+
+
+
+
